d881b33db79d60014d42aa4ab58d74747e5ead14
Felix Kunjachan Lukose <kunjacha@sheridancollege.ca>
2024-01-24 14:47:40 -0500

    commit

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author felix
+ */
+public class PartTimeStudent extends Student{
+    private int numOfCourses;
+
+    public PartTimeStudent() {
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
